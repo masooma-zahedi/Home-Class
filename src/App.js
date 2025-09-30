6f7a0b93d634239e8170080f0d3797a5f2dc7bda
masooma zahedi <masooma.zahedi1@gmail.com>
2025-09-30 11:36:48 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import MainLayout from './layouts/MainLayout';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi masooma salam </h1>
+      <MainLayout/>
     </div>
   );
 }
