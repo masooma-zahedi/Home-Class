30afc812fccaafaece8ecc9ddb8b086881c61fc3
masooma zahedi <masooma.zahedi1@gmail.com>
2025-09-30 15:42:35 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MainLayout from "./layouts/MainLayout";
-import SectionLayout from "./layouts/SectionLayout";
-import Home from "./pages/Home";
-import { CardAlphaCompo } from "./sections/alphabet/CardAlphaCompo";
+import './App.css';
+import MainLayout from './layouts/MainLayout';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <h1>Hi Masooma! Salam 🌸</h1>
-      </div>
-
-      <Routes>
-        {/* مسیر اصلی */}
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-
-          {/* بخش آموزش - الفبا */}
-          <Route
-            path="alphabet"
-            element={
-              <SectionLayout
-                basePath="/alphabet"
-                menuItems={[{ label: "کارت الفبا", path: "" }]}
-              />
-            }
-          >
-            <Route path="" element={<CardAlphaCompo />} />
-          </Route>
-
-          {/* در آینده می‌توانید مسیرهای دیگر اضافه کنید */}
-        </Route>
-      </Routes>
-    </Router>
+    <div className="App">
+      <h1>Hi masooma salam </h1>
+      <MainLayout/>
+    </div>
   );
 }
 
