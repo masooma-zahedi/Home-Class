abde18cea07a56f22a647faca180e3ff243c4c8e
masooma zahedi <masooma.zahedi1@gmail.com>
2025-11-11 19:09:33 -0600

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,16 @@ import StoryPage3 from "../sections/story/StoryPage3";
 import DragDropQuiz from "../sections/word/DragDropQuiz";
 import BouncingBalls from "../sections/alphabet/BouncingBalls";
 import ObjectSentenceGame from "../sections/alphabet/ObjectSentenceGame";
+import WordFlipBox from "../sections/alphabet/WordFlipBox";
 
 export default function Home() {
   return (<>
-  <h4 className="bg-warning p-2 text-center rounded my-2 mx-1" style={{color:" #772245"}}>بچه ها حرف ق را یاد گرفتند. باقی داستان خوانده شد و قسمت پایین عکس داستان را بچه ها بنویسند. جمله سازی با تصویر را هم تمرین کنند .</h4>
-    <ObjectSentenceGame/>
+  <h4 className="bg-warning p-2 text-center rounded my-2 mx-1" style={{color:" #772245"}}>بچه ها امروز حرف ه را یاد گرفتند و کلمات حرف ق را با هم تمرین کردیم. بچه ها از روی کلمات یک بار بخوانند و یک بار بنویسند .</h4>
+      <WordFlipBox/>
+    {/* <ObjectSentenceGame/> */}
     {/* <BouncingBalls/> */}
     {/* <BrokenGlass/> */}
-    <StoryPage3 groupKey="alefba" wantedTitle="دارویِ تَلخ" />
+    {/* <StoryPage3 groupKey="alefba" wantedTitle="دارویِ تَلخ" /> */}
     <CardAlphaCompo/> 
     {/* <DragDropQuiz/> */}
   </>)
