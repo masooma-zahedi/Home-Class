45110cc116221cc2fb98452ba9579248a7127ada
masooma zahedi <masooma.zahedi1@gmail.com>
2025-11-24 12:54:43 -0600

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,16 +6,24 @@ import DragDropQuiz from "../sections/word/DragDropQuiz";
 import BouncingBalls from "../sections/alphabet/BouncingBalls";
 import ObjectSentenceGame from "../sections/alphabet/ObjectSentenceGame";
 import WordFlipBox from "../sections/alphabet/WordFlipBox";
+import VideoCard from "../sections/alphabet/VideoCard";
 
 export default function Home() {
   return (<>
-  <h4 className="bg-warning p-2 text-center rounded my-2 mx-1" style={{color:" #772245"}}>بچه ها امروز حرف ه را یاد گرفتند و کلمات حرف ق را با هم تمرین کردیم. بچه ها از روی کلمات یک بار بخوانند و یک بار بنویسند .</h4>
-      <WordFlipBox/>
+  <h4 className="bg-warning p-2 text-center rounded my-2 mx-1" style={{color:" #772245"}}>بچه ها فیلم زیر را ببینند و فارسی آن را یاد بگیرند. داستان را دو خط اول را خواندیم ، آن را بخوانند .</h4>
+      {/* <WordFlipBox/> */}
+      <VideoCard
+        title="قصه کوتاه امروز"
+        description="متن فارسی داستان زیر را بخوانید."
+        videoFileName={`${process.env.PUBLIC_URL}/video/bigThings.mp4`}  // فقط اسم فایل ویدیوی mp4
+      />
+
     {/* <ObjectSentenceGame/> */}
     {/* <BouncingBalls/> */}
     {/* <BrokenGlass/> */}
-    {/* <StoryPage3 groupKey="alefba" wantedTitle="دارویِ تَلخ" /> */}
+    <StoryPage3 groupKey="alefba" wantedTitle="گُلدانِ نازنین" />
     <CardAlphaCompo/> 
     {/* <DragDropQuiz/> */}
+
   </>)
 }
