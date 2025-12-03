40ff7ad6e7b4ac600c7fe23a96e297ed95d60c25
masooma zahedi <masooma.zahedi1@gmail.com>
2025-12-02 19:36:01 -0600

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,25 +8,27 @@ import ObjectSentenceGame from "../sections/alphabet/ObjectSentenceGame";
 import WordFlipBox from "../sections/alphabet/WordFlipBox";
 import VideoCard from "../sections/alphabet/VideoCard";
 import DragWriteBoxWithGuides from "../sections/alphabet/DragWriteBoxWithGuides ";
+import MatchingLettersPicturs from "../sections/alphabet/MatchingLettersPicturs";
 
 export default function Home() {
   return (<>
-  <h4 className="bg-warning p-2 text-center rounded my-2 mx-1" style={{color:" #772245"}}>بچه ها حرف ض را یاد گرفتند و کلمات حرف ه را خواندیم . از روی کلمات 15 تا را بنویسند .</h4>
+  <h4 className="bg-warning p-2 text-center rounded my-2 mx-1" style={{color:" #772245"}}>بچه ها از روی کلمات حرف ض یکبار بنویسند و فعالیت تطبیق حرف با تصویر را انجام دهند .</h4>
       {/* <WordFlipBox/> */}
+
     <BrokenGlass/>
-      <VideoCard
+      {/* <VideoCard
         title="قصه کوتاه امروز"
         description="متن فارسی داستان زیر را بخوانید."
         videoFileName={`${process.env.PUBLIC_URL}/video/bigThings.mp4`}  // فقط اسم فایل ویدیوی mp4
-      />
-
+      /> */}
+    <MatchingLettersPicturs/>
+    <StoryPage3 groupKey="alefba" wantedTitle="گُلدانِ نازنین" />
     {/* <ObjectSentenceGame/> */}
     {/* <BouncingBalls/> */}
     <div className="container  mt-4" dir="rtl">
-      <DragWriteBoxWithGuides width={840} height={520} showGuides={true} downloadName="persian_practice.png" textTitle="آیلین و سِتایِش هَر روز به مَدرِسه می روند."/>
+      <DragWriteBoxWithGuides width={840} height={520} showGuides={true} downloadName="persian_practice.png" textTitle="آیلین و سِتایِش هَر روز با هَم بازی می کُنَند."/>
     </div>
 
-    <StoryPage3 groupKey="alefba" wantedTitle="گُلدانِ نازنین" />
     <CardAlphaCompo/> 
     {/* <DragDropQuiz/> */}
   </>)
