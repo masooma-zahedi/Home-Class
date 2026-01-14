e8bbc7549dca8da04dc4be9945afff062ead8dfb
masooma zahedi <masooma.zahedi1@gmail.com>
2026-01-13 19:05:31 -0600

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,21 +14,21 @@ import WordGameWithCategories from "../sections/alphabet/WordGameWithCategories"
 
 export default function Home() {
   return (<>
-  <h4 className="bg-warning p-2 text-center rounded my-2 mx-1" style={{color:" #1a2099ff"}}>بچه ها حرف ع را یاد گرفتند . کلمات حرف ع را بخوانند و از روی 10 کلمه آن بنویسند .</h4>
+  <h4 className="bg-warning p-2 text-center rounded my-2 mx-1" style={{color:" #1a2099ff"}}>بچه ها کلمات و جملات زیر را بخوانند و از روی 10 کلمه آن بنویسند .</h4>
       {/* <ObjectSentenceGame/> */}
 
       {/* <WordFlipBox/> */}
     {/* <WordGameWithCategories initialCategory="زمستان" /> */}
-      {/* <VideoCard
+        <SylabeWords/>
+      <VideoCard
         title="قصه کوتاه امروز"
         description="متن فارسی داستان زیر را بخوانید."
-        videoFileName={`${process.env.PUBLIC_URL}/video/bigThings.mp4`}  // فقط اسم فایل ویدیوی mp4
-      /> */}
+        videoFileName={`${process.env.PUBLIC_URL}/video/big&big.mp4`}  // فقط اسم فایل ویدیوی mp4
+      />
     {/* <MatchingLettersPicturs/> */}
-    {/* <SylabeWords/> */}
     {/* <BrokenGlass/> */}
-    <BouncingBalls/>
-    <StoryPage3 groupKey="alefba" wantedTitle="اُمید و ظَرفِ غَذا" />
+    {/* <BouncingBalls/> */}
+    {/* <StoryPage3 groupKey="alefba" wantedTitle="اُمید و ظَرفِ غَذا" /> */}
 
     {/* <div className="container  mt-4" dir="rtl">
       <DragWriteBoxWithGuides width={840} height={520} showGuides={true} downloadName="persian_practice.png" textTitle="آیلین و سِتایِش هَر روز با هَم بازی می کُنَند."/>
