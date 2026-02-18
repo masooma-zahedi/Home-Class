08bbdb13d724e357aee01a20fe05623624f4b132
masooma zahedi <masooma.zahedi1@gmail.com>
2026-02-17 20:21:58 -0600

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ import SentenceObject from "../sections/word/SentenceObject";
 
 export default function Home() {
   return (<>
-  <h4 className="bg-warning p-2 text-center rounded my-2 mx-1" style={{color:" #1a2099ff"}}>بچه ها از روی داستان زیر بخوانند و معنی کلمات را یاد بگیرند و تا بالای عکس از روی داستان بنویسند .</h4>
+  <h4 className="bg-warning p-2 text-center rounded my-2 mx-1" style={{color:" #1a2099ff"}}>بچه ها از روی داستان زیر بخوانند و معنی کلمات را یاد بگیرند و قسمت پایین عکس داستان را بنویسند .</h4>
       {/* <ObjectSentenceGame/> */}
       {/* <SentenceObject/> */}
 
@@ -30,7 +30,7 @@ export default function Home() {
     {/* <MatchingLettersPicturs/> */}
     {/* <BrokenGlass/> */}
     {/* <BouncingBalls/> */}
-    <StoryPage3 groupKey="alefba" wantedTitle="مِهمانیِ شادی" />
+    <StoryPage3 groupKey="alefba" wantedTitle="طوفانِ بُزرگ" />
 
     {/* <div className="container  mt-4" dir="rtl">
       <DragWriteBoxWithGuides width={840} height={520} showGuides={true} downloadName="persian_practice.png" textTitle="آیلین و سِتایِش هَر روز با هَم بازی می کُنَند."/>
