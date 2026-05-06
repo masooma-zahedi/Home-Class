4980552222cae1fb35a2a540be0ad6cee9ca62c5
masooma zahedi <masooma.zahedi1@gmail.com>
2026-05-05 19:02:40 -0500

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,18 +16,18 @@ import TypingImageText from "../sections/word/TypingImageText";
 
 export default function Home() {
   return (<>
-  <h4 className="bg-warning p-2 text-center rounded my-2 mx-1" style={{color:" #1a2099ff"}}>بچه ها داستان زیر را بخوانند و از روی 3 جمله آن بنویسند .</h4>
+  <h4 className="bg-warning p-2 text-center rounded my-2 mx-1" style={{color:" #1a2099ff"}}>بچه ها داستان زیر را بخوانند و از روی 5 جمله آن بنویسند .</h4>
       {/* <ObjectSentenceGame/> */}
       {/* <SentenceObject/> */}
       {/* <TypingImageText/> */}
       {/* <WordFlipBox/> */}
     {/* <WordGameWithCategories initialCategory="زمستان" /> */}
         {/* <SylabeWords/> */}
-      {/* <VideoCard
+      <VideoCard
         title="قصه کوتاه امروز"
         description="متن فارسی داستان زیر را بخوانید."
-        videoFileName={`${process.env.PUBLIC_URL}/video/big&big.mp4`}  // فقط اسم فایل ویدیوی mp4
-      /> */}
+        videoFileName={`${process.env.PUBLIC_URL}/video/fox&crew.mp4`}  // فقط اسم فایل ویدیوی mp4
+      />
     <StoryPage3 groupKey="alefba" wantedTitle="2-لیا و سوت زدن" />
     {/* <MatchingLettersPicturs/> */}
     {/* <BrokenGlass/> */}
